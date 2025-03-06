448ed03882236574287eefac87aa3b5833b7bf4c
aditya dixit <adirkt2003@gmail.com>
2025-03-06 16:30:15 +0530

    Set up for Address Book

---
+++ src/main/java/com/example/AddressBookMap/AddressBookMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.AddressBookMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AddressBookMap/AddressBookMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AddressBookMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
